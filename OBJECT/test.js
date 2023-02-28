fcd587e92d4b5fce1ae4545fc85e8ceac29169a2
Ehson Zokirov <zokirov_2000@mail.ru>
2023-02-28 14:07:49 +0500

    asd

--- OBJECT/test.js
+++ OBJECT/test.js
@@ -1,3 +1,3 @@
 const person = { name: "John", age: "20" };
-console.log(Object.entries(person));
-// [ [ 'name', 'John' ], [ 'age', '20' ] ]
+console.log(Object.values(person));
+// [ 'John', '20' ]
